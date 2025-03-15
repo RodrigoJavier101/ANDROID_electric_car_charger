f6b4d595499486e4a58de411c442300eda9ec675
rj <rj@gigioberto.com>
2025-03-15 19:00:07 -0300

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,36 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+//import java.util.Properties
+
+//val properties: Properties = Properties().apply {
+//    rootProject.file("local.properties").reader().use(::load)
+//}
+
+
+//properties.load(
+//    project.rootProject
+//        .file("local.properties")
+//        .inputStream()
+//)
+
+val prop: Properties = Properties()
+
+prop.load(
+    project.rootProject
+        .file("local.properties")
+        .inputStream()
+)
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+// asdded
+    alias(libs.plugins.android.dagger.hilt)
+//   alias(libs.plugins.ksp)
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -27,6 +56,48 @@ android {
             )
         }
     }
+    flavorDimensions += "version"
+    productFlavors {
+
+        create("Charger") {
+            applicationIdSuffix = ".Charger"
+            versionNameSuffix = "1_Charger"
+
+            resValue(
+                "string",
+                "key_map",
+                prop.getProperty("API_MAP_KEY")
+
+            )
+
+            resValue(
+                "string",
+                "app_name",
+                "Charger"
+            )
+
+            buildConfigField(
+                "String",
+                "URL_BASE",
+                prop.getProperty("URL_BASE")
+            )
+
+            buildConfigField(
+                "String",
+                "URL_ENDPOINT",
+                prop.getProperty("URL_ENDPOINT")
+            )
+
+            buildConfigField(
+                "String",
+                "COMPANY_ID",
+                prop.getProperty("COMPANY_ID")
+            )
+
+        }
+
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,6 +107,9 @@ android {
     }
     buildFeatures {
         compose = true
+        //
+        buildConfig = true
+        viewBinding = true
     }
 }
 
@@ -56,4 +130,83 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+// // // // //
+    // TEST
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    /* TESTS ADDED */
+    testImplementation(libs.mokk)
+    testImplementation(libs.mockitocore)
+    testImplementation(libs.mockitoinline)
+
+    /*splashscreen*/
+    implementation(libs.androidx.splash)
+
+    /*google map*/
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.android.maps.utils)
+
+    /*navigation NO CAMBIAR LA VERSION "2.6.0" **************/
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.fragmentktx)
+    implementation(libs.androidx.legacy)
+
+    /*picasso*/
+    implementation(libs.androidx.picasso)
+    // constraint layout
+//    implementation(libs.androidx.constraintlayout)
+    //    retrofit
+    implementation(libs.androidx.retrofit)
+    implementation(libs.androidx.retrofitgson)
+// okhttp
+    implementation(libs.okhttp)
+    implementation(libs.googlegson)
+    implementation(libs.okhttp.interceptor)
+
+    //    coroutines
+    implementation(libs.androidx.coroutines)
+    implementation(libs.androidx.coroutinesandroid)
+
+    //   coroutine lifecycle scopes
+    implementation(libs.androidx.activityktx)
+    implementation(libs.androidx.lifecyclesextensions)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // KTX - Viewmodel Y Livedata NO ACTUALIZAR: 2.5.1
+    implementation(libs.androidx.lifecycle.livedata)
+    //    data store
+    implementation(libs.androidx.datastore)
+    //Room
+    implementation(libs.room)
+    // kapt genera el codigo para la base de datos
+    kapt(libs.roomkaptcompiler)
+    implementation(libs.androidx.room.runtime)
+
+    //Hilt
+    implementation(libs.dagger.google)
+    implementation(libs.androidx.hilt.navigation.compose.v120)
+    annotationProcessor(libs.androidx.google.compiler)
+    kapt(libs.androidx.google.dagger.kapt.android)
+    kapt(libs.androidx.google.compiler)
+    kapt(libs.androidx.hilt.compiler.v120)
+
+    // shimmer
+    implementation(libs.shimmer)
+
+//   COMPOSE
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.runtime)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+
 }
+
+kapt { correctErrorTypes = true }
